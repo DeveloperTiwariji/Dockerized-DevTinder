8c6fc0df04231241427677f46acd0b9b2a8ad069
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2025-04-28 10:51:28 +0530

    Error Fixing

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -7,8 +7,11 @@ const cors = require("cors");
 require('dotenv').config();
 
 
+// app.use(cors({ origin: true, credentials: true }));
+
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     credentials: true,
 }));
 app.use(express.json());
